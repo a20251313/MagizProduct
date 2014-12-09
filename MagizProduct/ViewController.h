2a6394eead8482b217a57abf9343b91778a1f368
a20251313 <a20251313@163.com>
2014-12-09 10:51:22 +0800

    Initial Commit

---
+++ MagizProduct/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagizProduct
+//
+//  Created by jingfuran on 14/12/9.
+//  Copyright (c) 2014年 jingfuran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
